f4e121086dc3c32c25bace32a119e7a2f532412d
Sir-Turtle <tolgaakkaya@live.ca>
2016-12-28 13:03:36 -0500

    Added comments to both classes Rot13 & Helper, in addition to a private constructor to Helper.

--- src/Helper.java
+++ src/Helper.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 /**
  * The purpose of this class is to provide convenient methods for reading input
@@ -12,19 +11,44 @@ import java.util.StringTokenizer;
 
 public class Helper {
 	private static final BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-
+	
+	// The private constructor is so that instances of this class cannot be constructed.
+	private Helper(){}
+	
+	/**
+	 * Read an array of char values from the keyboard.
+	 * 
+	 * After the user has entered a string of characters, hitting 'enter'
+	 * returns to the calling method. All characters including spaces are put
+	 * in the array.
+	 *
+	 * The String entered by the user is converted to an array of characters of
+	 * the appropriate length.
+	 * 
+	 * @return An array of char values containing the set
+	 *	of characters entered from the keyboard.
+	 */
 	public static char[] readCharLine() {
+		// Get the user's input string.
 		String inputString = getInput();
-
+		
+		// Convert the entire String that was entered to an array of chars.
 		return inputString.toCharArray();
 	}
-
-	public static String readString() {
-		String inputString = getInput();
-
-		return inputString;
-	}
-
+	
+	/**
+	 * This method gets the input from the keyboard as a String.
+	 * 
+	 * The intention of this method is to catch the possible {@link IOException}
+	 * that could be thrown by the readline() method, so that the
+	 * main() method does not have to declare that it throws this
+	 * exception.
+	 * 
+	 * If the exception occurs, a stack trace is printed and the program is
+	 * terminated.
+	 * 
+	 * @return A {@link String} with the input typed from the keyboard.
+	 */
 	private static String getInput() {
 		String inputString = null;
 		try {
--- src/Rot13.java
+++ src/Rot13.java
@@ -6,24 +6,31 @@
 
 public class Rot13 {
 	public static void main(String[] args) {
-
+		
 		char[] strFromUser;
-
+		
+		// Request the user's input string.
 		System.out.println("Please enter a sentence to cipher.");
 		strFromUser = Helper.readCharLine();
+		
 		int n = strFromUser.length;
-
+		
+		// Call cipher method on input string.
 		if (n >= 1) {
 			cipher(strFromUser, n);
 		}
-
+		
+		// Printing ciphered string.
 		for (int i = 0; i < strFromUser.length; i++) {
 			System.out.print(strFromUser[i]);
 		}
 
 	}
 
+	// Method: cipher
+	// Description: Ciphers a sentence provided by the user.
 	public static void cipher(char[] strFromUser, int n) {
+		// Recursively cipher input string.
 		if (n == 1) {
 			strFromUser[0] = rot13(strFromUser[0]);
 		} else {
@@ -33,10 +40,13 @@ public class Rot13 {
 
 	}
 
+	// Method: rot13
+	// Description: Using the rot 13 technique, characters within the alphabet are translated into other characters.
 	public static char rot13(char ch) {
 		int pos = (int) ch;
 		int pos2 = 0;
-
+		
+		// Check if characters exist in alphabet, then evaluate.
 		if (('A' <= ch) && (ch <= 'Z') || ('a' <= ch) && (ch <= 'z')) {
 			if (('A' <= ch) && (ch <= 'Z')) {
 				pos = pos - 'A';
@@ -51,7 +61,8 @@ public class Rot13 {
 		} else {
 			pos2 = pos;
 		}
-
+		
+		// Final ciphered result.
 		return ((char) pos2);
 	}
 }
